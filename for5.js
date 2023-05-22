3cdc63e064ce9ee6561ef734b7a6cb5988f82475
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-05-21 22:17:45 -0300

    for 5 completado

--- for5.js
+++ for5.js
@@ -5,8 +5,15 @@ const selectOpcionesMeses = document.querySelector("#opciones-suscripcion");
 function generarOpciones() {
     let cantMeses = inputCantMeses.value;
     selectOpcionesMeses.innerHTML="";
-    
+
     for (let i = 1; i <= cantMeses; i++) {
+    
+    if (i==1) {
+        selectOpcionesMeses.innerHTML += `
+            <option value="">Seleccione</option>
+        `;
+            
+        }
         selectOpcionesMeses.innerHTML += `
             <option value="">Meses ${i}</option>
         `;
